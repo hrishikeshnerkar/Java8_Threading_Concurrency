79d31e16bc83b23758c75dc551966136dcbae867
Nerkar <2388092@cognizant.com>
2025-08-19 14:32:07 +0530

    commit

--- com.example/src/MethodReferenceApp/MethodReferenceApp.java
+++ com.example/src/MethodReferenceApp/MethodReferenceApp.java
@@ -1,5 +1,6 @@
 package MethodReferenceApp;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class MethodReferenceApp {
@@ -11,9 +12,22 @@ public class MethodReferenceApp {
                 new Person("John", 56),
                 new Person("Anna", 12));
 
+        System.out.println("Using age");
         people.stream()
                 .sorted((person1, person2) -> person1.getAge().compareTo(person2.getAge())) // Or else we can use .sorted(Comparator.comparing(Person::getAge))
                 .forEach(System.out::println);
 
+        System.out.println();
+        System.out.println("Using name but on same name we check for age");
+        people.stream()
+                .sorted(Comparator.comparing(Person::getName).thenComparing(Person::getAge))
+                .forEach(System.out::println);
+
+        System.out.println();
+        System.out.println("Method reference using object");
+        PersonComparisonProvider comparisonProvider = new PersonComparisonProvider();
+        people.stream()
+                .sorted(comparisonProvider::compareByNameAndAge)
+                .forEach(System.out::println);
     }
 }
---
+++ com.example/src/MethodReferenceApp/PersonComparisonProvider.java
@@ -0,0 +1,11 @@
+package MethodReferenceApp;
+
+import java.util.Comparator;
+
+public class PersonComparisonProvider {
+    public int compareByNameAndAge(Person person1, Person person2){
+        return Comparator.comparing(Person::getName)
+                .thenComparing(Person::getAge)
+                .compare(person1, person2);
+    }
+}
