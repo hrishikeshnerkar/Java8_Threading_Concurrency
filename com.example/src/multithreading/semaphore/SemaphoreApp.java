ef3787009366b0b229a4a4afd274db691d4d4710
Hrishikesh <hrishikeshnerkar25@gmail.com>
2025-09-08 14:59:17 +0530

    Added mutex, semaphore

---
+++ com.example/src/multithreading/semaphore/SemaphoreApp.java
@@ -0,0 +1,17 @@
+package multithreading.semaphore;
+
+public class SemaphoreApp {
+    public static void main(String[] args) {
+        ParkingLot parkingLot = new ParkingLot(5);
+
+        Runnable parkCar = () -> {
+            for (int i = 0; i < 2; i++) {
+                parkingLot.park();
+            }
+        };
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(parkCar).start();
+        }
+    }
+}
